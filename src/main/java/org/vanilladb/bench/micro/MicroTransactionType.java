c67fbd282445eba820a18890b06033f89868f051
ctsai <ctsai@netdb.cs.nthu.edu.tw>
2017-07-09 13:10:09 +0800

    Add Migration Type

--- src/main/java/org/vanilladb/bench/micro/MicroTransactionType.java
+++ src/main/java/org/vanilladb/bench/micro/MicroTransactionType.java
@@ -5,21 +5,24 @@ import org.vanilladb.bench.TransactionType;
 public enum MicroTransactionType implements TransactionType {
 	// Loading procedures
 	SCHEMA_BUILDER, TESTBED_LOADER,
-	
+
 	// Profiling
 	START_PROFILING, STOP_PROFILING,
-	
+
 	// TPC-C procedures
-	MICRO;
-	
+	MICRO,
+
+	// Migration
+	START_MIGRATION, STOP_MIGRATION, MIGRATION_ANALYSIS, ASYNC_MIGRATE;
+
 	public static MicroTransactionType fromProcedureId(int pid) {
 		return MicroTransactionType.values()[pid];
 	}
-	
+
 	public int getProcedureId() {
 		return this.ordinal();
 	}
-	
+
 	public boolean isBenchmarkingTx() {
 		if (this == MICRO)
 			return true;
